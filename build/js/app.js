61a7e31df7c0cfe7b8fa9d8d00c801a31052628e
Frank <faddelia@gmail.com>
2021-07-23 23:14:31 -0500

    Calculate tip amount

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,15 @@
+const bill = document.querySelector('#bill');
 const buttons = document.querySelectorAll('.btn-group .btn');
+const tipAmount = document.querySelector('#tipAmount');
 
+let billAmount = 0;
 let tipPercentage = 0;
+
+bill.addEventListener('keyup', () => {
+    billAmount = parseFloat(bill.value);
+
+    calculateTipAmount();
+});
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -8,6 +17,8 @@ buttons.forEach((button) => {
         button.classList.add('active');
 
         tipPercentage = button.dataset.tipPercentage;
+
+        calculateTipAmount();
     })
 });
 
@@ -15,4 +26,11 @@ function clearActiveState() {
     buttons.forEach((button) => {
         button.classList.remove('active');
     });
+}
+
+function calculateTipAmount () {
+    if (billAmount >= 0) {
+        const total = billAmount * tipPercentage;
+        tipAmount.innerHTML = `\$${total.toFixed(2)}`;
+    }
 }
