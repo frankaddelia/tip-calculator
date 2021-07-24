f93f506fef40b61b9e21617c82e2b674c740e0b9
Frank <faddelia@gmail.com>
2021-07-24 15:38:50 -0500

    Add reset button functionality

--- build/js/app.js
+++ build/js/app.js
@@ -2,6 +2,7 @@ const bill = document.querySelector('#bill');
 const buttons = document.querySelectorAll('.btn-group .btn');
 const customTip = document.querySelector('#custom-tip');
 const numPeople = document.querySelector('#numPeople');
+const reset = document.querySelector('#reset');
 const tipAmount = document.querySelector('#tipAmount');
 const total = document.querySelector('#total');
 
@@ -54,13 +55,25 @@ numPeople.addEventListener('change', () => {
     calculateTipAmount(numPeople.value);
 });
 
+reset.addEventListener('click', () => {
+    bill.value = '';
+    billAmount = 0;
+    customTip.value = '';
+    numPeople.value = '';
+    tipAmount.innerHTML = '$0.00';
+    tipPercentage = 0;
+    total.innerHTML = '$0.00';
+
+    clearActiveState();
+});
+
 function clearActiveState() {
     buttons.forEach((button) => {
         button.classList.remove('active');
     });
 }
 
-function calculateTipAmount (totalPerPerson = 1) {
+function calculateTipAmount(totalPerPerson = 1) {
     if (billAmount >= 0 && isNumeric(billAmount)) {
         const tipTotal = billAmount * tipPercentage;
         tipAmount.innerHTML = `\$${tipTotal.toFixed(2)}`;
