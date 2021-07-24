b00f9dd727dfd976f9614fe3c8f167af85410a0b
Frank <faddelia@gmail.com>
2021-07-24 16:20:14 -0500

    Fix active and error states for fields

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,9 @@
 const bill = document.querySelector('#bill');
+const billErrorLabel = document.querySelector('#bill-error-label');
 const buttons = document.querySelectorAll('.btn-group .btn');
 const customTip = document.querySelector('#custom-tip');
 const numPeople = document.querySelector('#numPeople');
+const numPeopleErrorLabel = document.querySelector('#numPeople-error-label');
 const reset = document.querySelector('#reset');
 const tipAmount = document.querySelector('#tipAmount');
 const total = document.querySelector('#total');
@@ -10,8 +12,22 @@ let billAmount = 0;
 let tipPercentage = 0;
 
 bill.addEventListener('keyup', () => {
+    if (!isNumeric(bill.value) || bill.value < 0) {
+        billErrorLabel.classList.remove('hidden');
+        bill.classList.add('input-error-box');
+        return;
+    }
+
     billAmount = parseFloat(bill.value);
-    
+
+    if (bill.classList.contains('input-error-box')) {
+        bill.classList.remove('input-error-box');
+    }
+
+    if (! billErrorLabel.classList.contains('hidden')) {
+        billErrorLabel.classList.add('hidden');
+    }
+
     calculateTipAmount();
 });
 
@@ -38,9 +54,13 @@ customTip.addEventListener('keyup', () => {
 });
 
 numPeople.addEventListener('keyup', () => {
-    if (!isNumeric(numPeople.value)) {
-        console.error('not numeric!', numPeople.value);
+    if (numPeople.value < 1) {
+        numPeopleErrorLabel.classList.remove('hidden');
         return;
+    }
+
+    if (! numPeopleErrorLabel.classList.contains('hidden')) {
+        numPeopleErrorLabel.classList.add('hidden')
     }
 
     calculateTipAmount(numPeople.value);
@@ -48,8 +68,11 @@ numPeople.addEventListener('keyup', () => {
 
 numPeople.addEventListener('change', () => {
     if (!isNumeric(numPeople.value)) {
-        console.error('not numeric!', numPeople.value);
         return;
+    }
+
+    if (! numPeopleErrorLabel.classList.contains('hidden')) {
+        numPeopleErrorLabel.classList.add('hidden')
     }
 
     calculateTipAmount(numPeople.value);
